ea12d0f65ba1e2ddc73e9536d44af0da28b4dd29
Devashish Upreti <devashup68@gmail.com>
2025-05-30 13:05:02 +0545

    users table created

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,9 @@
 import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
+  route("sign-in", "routes/root/SignIn.tsx"),
   layout("routes/admin/AdminLayout.tsx", [
     route("dashboard", "routes/admin/Dashboard.tsx"),
-    route("all-users", "routes/admin/AllUsers.tsx")
+    route("all-users", "routes/admin/AllUsers.tsx"),
   ]),
 ] satisfies RouteConfig;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,10 +1,12 @@
 import { Header, StatsCard, TripCard } from "components";
+import { getUser } from "~/appwrite/auth";
 import { allTrips, dashboardStats, user, users } from "~/constants";
-
+import type { Route } from "./+types/Dashboard";
 const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
   dashboardStats;
-
-const Dashboard = () => {
+export const clientLoader = async () => await getUser();
+const Dashboard = ({ loaderData }: Route.ComponentProps) => {
+  const user = loaderData as User | null;
   return (
     <main className="dashboard wrapper">
       <Header
