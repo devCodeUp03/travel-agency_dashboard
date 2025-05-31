f8d9d1c4232a4314a75ecc3b7aa07ba60fc42a69
Devashish Upreti <devashup68@gmail.com>
2025-05-31 13:16:08 +0545

    created a gemini generating trip

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,12 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
   route("sign-in", "routes/root/SignIn.tsx"),
+  route("api/create-trip", "routes/api/create-trip.ts"),
   layout("routes/admin/AdminLayout.tsx", [
     route("dashboard", "routes/admin/Dashboard.tsx"),
     route("all-users", "routes/admin/AllUsers.tsx"),
+    route("trips", "routes/admin/Trips.tsx"),
+    route("trips/create", "routes/admin/CreateTrip.tsx"),
+    route("trips/:tripId", "routes/admin/TripDetails.tsx")
   ]),
 ] satisfies RouteConfig;
