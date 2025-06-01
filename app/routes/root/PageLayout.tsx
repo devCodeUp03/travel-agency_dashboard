540867a43f2c790a861a3c8c08301a15567426fa
Devashish Upreti <devashup68@gmail.com>
2025-06-02 00:19:59 +0545

    pagelayout updated

--- app/routes/root/PageLayout.tsx
+++ app/routes/root/PageLayout.tsx
@@ -8,10 +8,12 @@ const PageLayout = () => {
     navigate("/sign-in");
   };
   return (
-    <div>
+    <div className="flex gap-4 h-[100vh] items-center justify-center">
       <button onClick={handleLogout} className="cursor-pointer">
         <img src="/assets/icons/logout.svg" alt="logout" className="size-6" />
+        
       </button>
+      <h1 className="text-4xl font-semibold">ONLY <span className="font-extrabold text-green-700">ADMIN</span> CAN VISIT ADMIN DASHBOARD</h1>
     </div>
   );
 };
